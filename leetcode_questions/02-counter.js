23883d8a40042fec9ef1016af2d3557490a34a88
7vikfox <miw6515@gmail.com>
2026-04-27 18:05:23 +0530

    Add createCounter function to return incrementing counter
    Implement createCounter function that increments n.

--- leetcode_questions/02-counter.js
+++ leetcode_questions/02-counter.js
@@ -1,3 +1,13 @@
+//Counter Function
+//📌 Problem
+
+//Given an integer n, return a counter function.
+
+//This counter function should:
+
+//Initially return n
+//Then return n + 1, n + 2, n + 3, ... on each subsequent call
+
 var createCounter = function(n) {
     return function() {
         return n++;
