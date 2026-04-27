e32e709bc85e8c4b8fa13482c08faab6b8a8220c
7vikfox <miw6515@gmail.com>
2026-04-27 18:07:15 +0530

    Implement createCounter function with basic structure
    Add initial implementation of createCounter function.

--- leetcode_questions/04-createCounter.js
+++ leetcode_questions/04-createCounter.js
@@ -1,3 +1,18 @@
+//createCounter (Object Version)
+//📌 Problem
+
+//Write a function createCounter.
+
+//It should accept an initial integer init and return an object with three functions:
+
+//Functions:
+//increment()
+//Increases value by 1 and returns it
+//decrement()
+//Decreases value by 1 and returns it
+//reset()
+//Resets value back to initial init and returns it
+
 var createCounter = function(init) {
     let current = init;
 
