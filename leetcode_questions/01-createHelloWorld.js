42647e47f8332732f699a51f08ba95a195641c98
7vikfox <miw6515@gmail.com>
2026-04-27 18:00:08 +0530

    Add createHelloWorld function
    Implement createHelloWorld function that returns 'Hello World'.

---
+++ leetcode_questions/01-createHelloWorld.js
@@ -0,0 +1,8 @@
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    };
+};
+
+// QUESTION : Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
