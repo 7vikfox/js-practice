4e6927e1efaae9557b63a65e335cc93c7969b7bd
7vikfox <miw6515@gmail.com>
2026-04-27 18:06:27 +0530

    Update 03-expect.js

--- leetcode_questions/03-expect.js
+++ leetcode_questions/03-expect.js
@@ -1,3 +1,18 @@
+//expect Function
+//📌 Problem
+
+//Write a function expect that helps developers test their code.
+
+//It should take a value val and return an object with two functions:
+
+//Functions:
+//toBe(val2)
+//Returns true if val === val2
+//Otherwise throws "Not Equal"
+//notToBe(val2)
+//Returns true if val !== val2
+//Otherwise throws "Equal"
+
 var expect = function(val) {
     return {
         toBe: function(otherVal) {
