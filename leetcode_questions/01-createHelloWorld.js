6329ec17d77242e395a58a26e81261fc0f20d3a5
7vikfox <miw6515@gmail.com>
2026-04-27 18:04:36 +0530

    Update 01-createHelloWorld.js

--- leetcode_questions/01-createHelloWorld.js
+++ leetcode_questions/01-createHelloWorld.js
@@ -1,8 +1,16 @@
+//📌 Problem
+
+//Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+//💡 Details
+//The returned function should ignore all inputs
+//It must always return the string "Hello World"
+
 var createHelloWorld = function() {
     return function(...args) {
         return "Hello World";
     };
 };
 
-// QUESTION : Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
  
